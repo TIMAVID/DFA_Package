aff4cc9757e404fd96c88eb92af5fddc8067dc0b
DavidTLedesma <ledesma-david@utexas.edu>
2019-05-14 11:01:44 -0500

    c

--- DFA/R/DFA_Plot_function.R
+++ DFA/R/DFA_Plot_function.R
@@ -10,7 +10,7 @@
 DFA_plot <- function(data) {
   v <- as.data.frame(data)
   g<- ggplot2::ggplot(v, aes(v$x.LD1, v$x.LD2)) +
-    geom_point(ggplot2::aes(color = v$class))
+    ggplot2::geom_point(ggplot2::aes(color = v$class))
   g <- g + ggplot2::ggtitle("LDA Predictions")
   g <- g + ggplot2::xlab("LD1")
   g <- g + ggplot2::ylab("LD2")
